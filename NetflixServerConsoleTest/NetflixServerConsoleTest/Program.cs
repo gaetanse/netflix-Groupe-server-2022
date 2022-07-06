6036e043f74d11d6d4a54ceaa6d2870e3629cc9d
gaetanse <gs99@hotmail.com>
2022-07-06 16:08:15 +0200

    add project console test

---
+++ NetflixServerConsoleTest/NetflixServerConsoleTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using NetflixServer.Classes;
+
+namespace NetflixServer
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Netflix netflix = new Netflix();
+            Console.WriteLine(SecurityPassword.Hash("mdp"));
+        }
+
+    }
+}
---
+++ NetflixServerConsoleTest/NetflixServerConsoleTest/classes/SecurityPassword.cs
@@ -0,0 +1,11 @@
+namespace NetflixServer.Classes
+{
+    public static class SecurityPassword
+    {
+        public static string Hash(string content)
+        {
+            //return the hash content using sha256
+            return "";
+        }
+    }
+}
