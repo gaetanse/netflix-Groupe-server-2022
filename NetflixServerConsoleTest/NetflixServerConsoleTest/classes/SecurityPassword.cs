79e1f85f986b7e916d51550e1b1b0f15acc74f77
gaetanse <gs99@hotmail.com>
2022-07-06 16:47:58 +0200

    add hash 256

--- NetflixServerConsoleTest/NetflixServerConsoleTest/classes/SecurityPassword.cs
+++ NetflixServerConsoleTest/NetflixServerConsoleTest/classes/SecurityPassword.cs
@@ -1,3 +1,6 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace NetflixServer.Classes
 {
     public static class SecurityPassword
@@ -5,7 +8,13 @@ namespace NetflixServer.Classes
         public static string Hash(string content)
         {
             //return the hash content using sha256
-            return "";
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] sourceBytes = Encoding.UTF8.GetBytes(content);
+                byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
+                string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+                return hash;
+            }
         }
     }
 }
