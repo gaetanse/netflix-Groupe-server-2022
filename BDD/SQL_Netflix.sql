3a32f54d253526af83a4a36769f3f049cd89e7cd
chris <test@test.com>
2022-07-06 16:37:37 +0200

    modif BDD anglais

--- BDD/SQL_Netflix.sql
+++ BDD/SQL_Netflix.sql
@@ -3,39 +3,39 @@
 CREATE TABLE FAQ (
 	id int not null PRIMARY KEY IDENTITY(1,1),
 	question varchar(100) not null,
-	reponse varchar(100) not null
+	response varchar(100) not null
 );
 
 DROP TABLE users
 
 CREATE TABLE users (
 	id int not null PRIMARY KEY IDENTITY(1,1),
-	nom varchar(100) not null,
-	prenom varchar(100) not null,
-	numeroStatut int not null,
+	lastname varchar(100) not null,
+	firstname varchar(100) not null,
+	numberstatut int not null,
 	mail varchar(100) not null,
-	mdp varchar(100) not null
+	[password] varchar(100) not null
 );
 
 DROP TABLE statut
 
 CREATE TABLE statut (
 	id int not null PRIMARY KEY IDENTITY(1,1),
-	nom varchar(100) not null
+	[name] varchar(100) not null
 );
 
 DROP TABLE ressources
 
 CREATE TABLE ressources (
 	id int not null PRIMARY KEY IDENTITY(1,1),
-	titre varchar(100) not null,
+	title varchar(100) not null,
 	categorie int not null,
 	tags varchar(200) not null,
 	[description] varchar(100) not null,
-	contenu varchar(200) not null,
-	trailerVideo varchar(200),
-	nbEpisode int,
-	nbSaison int
+	content varchar(200) not null,
+	trailervideo varchar(200),
+	nbepisode int,
+	nbseason int
 );
 
 DROP TABLE images
@@ -51,7 +51,7 @@ DROP TABLE tags
 
 CREATE TABLE tags (
 	id int not null PRIMARY KEY IDENTITY(1,1),
-	nom varchar(100) not null,
+	[name] varchar(100) not null,
 	[description] varchar(100) not null
 );
 
@@ -59,6 +59,6 @@ DROP TABLE categorie
 
 CREATE TABLE categorie (
 	id int not null PRIMARY KEY IDENTITY(1,1),
-	nom varchar(100) not null,
+	[name] varchar(100) not null,
 	[description] varchar(100) not null
 );
